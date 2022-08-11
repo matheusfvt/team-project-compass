66ca264975ec2f78d27ea7c6c234ac01228f573b
Matheus Favaretto <matheusfavaretto9@gmail.com>
2022-08-11 19:12:40 -0300

    Pagina 1 e 2 prontas com botao next

--- script/redirect-page.js
+++ script/redirect-page.js
@@ -1,6 +1,6 @@
-var button1 = document.getElementById("button-basic");
-var button2 = document.getElementById("button-social");
-var button3 = document.getElementById("button-certificates");
+var buttonB = document.getElementById("button-basic");
+var buttonS = document.getElementById("button-social");
+var buttonC = document.getElementById("button-certificates");
 
 var page1 = document.getElementById("first-page");
 var page2 = document.getElementById("second-page");
@@ -9,60 +9,39 @@ var page3 = document.getElementById("third-page");
 // button1.addEventListener("click", redirectPage1);
 // button2.addEventListener("click", redirectPage2);
 // button3.addEventListener("click", redirectPage3);
-var pages = document.querySelectorAll(".page");
-console.log(pages);
-
-console.log(page1);
-console.log(page2);
-console.log(page3);
-if(page1.classList.contains('active')){
-  console.log('tenho');
-}
 
 
 function resetPages() {
-  var pages = document.querySelectorAll(".page");
-  for (let i = 0; i < pages.length; i++) {
-    var el = pages[i];
-    if (el.classList.contains("active")) {
-      el.classList.remove("active");
+  console.log('testando reset pages');
+  page1.style.display = 'none';
+  page2.style.display = 'none';
+  page3.style.display = 'none';
+  var buttons = document.querySelectorAll(".button");
+  for (let i = 0; i < buttons.length; i++) {
+    var el = buttons[i];
+    if (el.classList.contains("bto-active")) {
+      el.classList.remove("bto-active");
     }
   }
 }
 
 function showFirstPage() {
   resetPages();
-  page1.classList.add('active');
+  buttonB.classList.add('bto-active');
+  page1.style.display = 'block';
+  // page1.classList.add('active');
 }
 
 function showSecondPage() {
   resetPages();
-  page2.classList.add('active');
+  buttonS.classList.add('bto-active');
+  page2.style.display = 'block';
+  // page2.classList.add('active');
 }
 
 function showThirdPage() {
   resetPages();
-  page3.classList.add('active');
+  buttonC.classList.add('bto-active');
+  page3.style.display = 'block';
+  // page3.classList.add('active');
 }
-
-
-
-
-
-// function redirectPage1() {
-//   page1.classList.add("active");
-//   page2.classList.remove("active");
-//   page3.classList.remove("active");
-// }
-
-// function redirectPage2() {
-//   page1.classList.remove("active");
-//   page2.classList.add("active");
-//   page3.classList.remove("active");
-// }
-
-// function redirectPage3() {
-//   page1.classList.remove("active");
-//   page2.classList.remove("active");
-//   page3.classList.add("active");
-// }
